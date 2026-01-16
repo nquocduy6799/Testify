93a47f17d1a4cf6bc6fe588cd131f2153a73d99f
Trần Hoàng Nhựt <hoangnhut666@hotmail.com>
2026-01-17 00:17:01 +0700

    Add project files.

---
+++ Testify/Components/Account/PasskeyOperation.cs
@@ -0,0 +1,8 @@
+namespace Testify.Components.Account
+{
+    public enum PasskeyOperation
+    {
+        Create = 0,
+        Request = 1,
+    }
+}
---
+++ Testify/Data/ApplicationUser.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Identity;
+
+namespace Testify.Data
+{
+    // Add profile data for application users by adding properties to the ApplicationUser class
+    public class ApplicationUser : IdentityUser 
+    {
+        public string ? FullName { get; set; }
+    }
+
+}
+
+
+        
