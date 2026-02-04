a31bbc71890b6e6c0b7c3881bbc88f5f68bec815
Trần Hoàng Nhựt <hoangnhut666@hotmail.com>
2026-02-05 02:05:27 +0700

    Add hierarchy support to TemplateCategory entity
    Added parent-child relationship properties to TemplateCategory, including ParentCategoryId, ParentCategory, and SubCategories. Also introduced a nullable Name property and cleaned up the TestSuiteTemplates navigation property.

--- Testify/Entities/TemplateCategory.cs
+++ Testify/Entities/TemplateCategory.cs
@@ -3,10 +3,15 @@
     public class TemplateCategory
     {
         public int Id { get; set; }
+
         public string? Name { get; set; }
         public string? Description { get; set; }
 
-        // Navigation property
-        public virtual ICollection<TestSuiteTemplate> TestSuiteTemplates { get; set; } = new List<TestSuiteTemplate>();
+        public int? ParentCategoryId { get; set; }
+        public virtual TemplateCategory? ParentCategory { get; set; }
+
+        public virtual ICollection<TemplateCategory> SubCategories { get; set; } = new List<TemplateCategory>();
+
+        public virtual ICollection<TestSuiteTemplate> TestSuiteTemplates { get; set; }= new List<TestSuiteTemplate>();
     }
 }
